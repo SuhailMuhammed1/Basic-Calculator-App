9880bf77a10e5a112063a3f6a8991da06196d259
SuhailMuhammed1 <suhailjr111@gmail.com>
2024-12-11 22:36:17 +0530

    updated debug and host

--- basic_calculator_app/basic_calculator_app/settings.py
+++ basic_calculator_app/basic_calculator_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!v(f)!q6djo@ezg)pcil#$)f3_3@@uv6wy#w+%p!)rujfvwy9('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
